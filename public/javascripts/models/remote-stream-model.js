1bce05234a843c2f38e92bed1abf6c18c9f361a5
mrganser <gersuarod@gmail.com>
2017-06-27 23:18:31 +0100

    some small fixes for better readabiity

--- public/javascripts/models/remote-stream-model.js
+++ public/javascripts/models/remote-stream-model.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //Model representing each remote stream, the important field is instant
-//which stores a value representing the volume of the stream
+//which stores a value representing the volume of the stream (RMS)
 function RemoteStream(id, stream, audioContext) {
   var self = this;
   self.id = id;
@@ -9,7 +9,6 @@ function RemoteStream(id, stream, audioContext) {
   self.script = audioContext.createScriptProcessor(2048, 1, 1);
   self.script.onaudioprocess = function(event) {
     var input = event.inputBuffer.getChannelData(0);
-    var output = event.outputBuffer.getChannelData(0);
     var sum = 0.0;
     for (var i = 0; i < input.length; ++i) {
       sum += input[i] * input[i];  
